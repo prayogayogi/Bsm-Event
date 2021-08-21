627c7e94b24ccabb1f66b41a2a367efbab9f1153
prayogayogi <yogikpiter@gmail.com>
2021-08-21 23:14:57 +0700

    add filed asset

---
+++ application/views/back/_includes/v_head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Dashboard - Voler Admin Dashboard</title>
+
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+
+	<link rel="stylesheet" href="assets/vendors/chartjs/Chart.min.css">
+
+	<link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
+	<link rel="stylesheet" href="assets/css/app.css">
+	<link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+</head>
